ad9ddeab6f7949eb7e8a2e17969a79907e5449f3
imrishabh18 <belikerishabh@gmail.com>
2026-05-13 20:50:48 +0530

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-const runframeRoot = "/Users/rishabhgupta/Public/tscircuit/runframe"
-const projectRoot =
-  "/Users/rishabhgupta/Public/tscircuit/arduino-uno-with-re-routing-demo"
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@tscircuit/runframe/runner": `${runframeRoot}/lib/runner.ts`,
-      lib: `${runframeRoot}/lib`,
-    },
     dedupe: ["react", "react-dom"],
-  },
-  server: {
-    fs: {
-      allow: [projectRoot, runframeRoot],
-    },
   },
 })
